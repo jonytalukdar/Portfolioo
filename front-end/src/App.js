f0654a7207c3a7133f074db2150ca455e5d7f0a1
jonytalukdar <jonytalukdar30@gmail.com>
2022-03-12 21:08:56 +0600

    completed navbar

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 
+import './app.scss';
+
 import { About, Skills, Testimonials, Header, Footer, Work } from './container';
 import { Navbar } from './components';
 
 const App = () => {
   return (
     <div className="app">
+      <Navbar />
       <Header />
       <About />
       <Work />
