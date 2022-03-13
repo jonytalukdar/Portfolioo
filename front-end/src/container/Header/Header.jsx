d8b42fd4f47616554db4536c7d8c28b06cf1830a
jonytalukdar <jonytalukdar30@gmail.com>
2022-03-13 14:33:00 +0600

    completed about sections

--- front-end/src/container/Header/Header.jsx
+++ front-end/src/container/Header/Header.jsx
@@ -60,7 +60,7 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className="app__header-circle"
       >
-        {[images.react, images.redux, images.javascript].map(
+        {[images.javascript, images.react, images.redux].map(
           (circle, index) => (
             <div className="circle-cmp app__flex" key={`circle/${circle}`}>
               <img src={circle} alt="circle" />
